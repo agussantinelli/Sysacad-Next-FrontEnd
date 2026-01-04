4c6af08379c9e49b82a9edf1b3239e3501e7acc3
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 19:46:59 -0300

    Aplico Servicio Nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeToggleComponent } from './shared/components/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
