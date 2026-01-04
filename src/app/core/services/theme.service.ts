e6a5515d2432f568cf444ba954ad384d02cc63e1
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 19:46:39 -0300

    Armado Service DarkMode

---
+++ src/app/core/services/theme.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ThemeService {
+    isDarkMode = signal<boolean>(false);
+
+    constructor() {
+        // Check system preference or localStorage
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme === 'dark') {
+            this.isDarkMode.set(true);
+            document.body.classList.add('dark-mode');
+        }
+    }
+
+    toggleTheme() {
+        this.isDarkMode.update(value => !value);
+        if (this.isDarkMode()) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light');
+        }
+    }
+}
