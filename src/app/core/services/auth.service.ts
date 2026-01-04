32792975e72ba8e89a2eafa32f87d2f8659d15c1
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 20:59:33 -0300

    Uso Variables Entorno

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,15 +2,14 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoginRequest, UsuarioResponse } from '../models/auth.models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     private http = inject(HttpClient);
-    // Assuming the backend is running on localhost:8080 or configurable. 
-    // Ideally this should be in environment files, but for now I'll use the URL from the controller's @CrossOrigin
-    private apiUrl = 'http://localhost:8080/api/auth';
+    private apiUrl = `${environment.apiUrl}/auth`;
 
     constructor() { }
 
