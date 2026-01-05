d3b6ae64c3c8bf1447e302af97a24d060152b2ec
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-05 02:42:46 -0300

    NUEVO: Interceptor Axios

---
+++ src/app/core/api/axios.client.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { environment } from '../../../environments/environment';
+
+const axiosClient = axios.create({
+    baseURL: environment.apiUrl,
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+axiosClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosClient;
