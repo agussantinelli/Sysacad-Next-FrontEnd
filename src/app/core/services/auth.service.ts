0ea8c84d41148bd39de1bfaf25656c2681ff24a4
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 22:37:51 -0300

    Logout Funciona

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { LoginRequest, UsuarioResponse } from '../models/auth.models';
 import { environment } from '../../../environments/environment';
 
@@ -11,9 +12,17 @@ export class AuthService {
     private http = inject(HttpClient);
     private apiUrl = `${environment.apiUrl}/auth`;
 
+    private router = inject(Router);
+
     constructor() { }
 
     login(credentials: LoginRequest): Observable<UsuarioResponse> {
         return this.http.post<UsuarioResponse>(`${this.apiUrl}/login`, credentials);
+    }
+
+    logout(): void {
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        this.router.navigate(['/']);
     }
 }
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ThemeService } from '../../../core/services/theme.service';
+import { ThemeService } from '../../core/services/theme.service';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
     selector: 'app-navbar',
@@ -11,14 +12,13 @@ import { ThemeService } from '../../../core/services/theme.service';
 })
 export class NavbarComponent {
     private themeService = inject(ThemeService);
+    private authService = inject(AuthService);
 
     get logoPath(): string {
         return this.themeService.isDarkMode() ? '/logo-utn-dark-mode.png' : '/logo-utn-light-mode.png';
     }
 
     logout(): void {
-        localStorage.removeItem('user');
-        localStorage.removeItem('token');
-        window.location.reload();
+        this.authService.logout();
     }
 }
