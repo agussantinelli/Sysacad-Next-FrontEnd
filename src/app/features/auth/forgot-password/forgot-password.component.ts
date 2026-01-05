ba55e955d12cfce7dc19c3a52360f0f24fd3dc99
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 23:01:37 -0300

    Incorporo Mensaje en Forgot

--- src/app/features/auth/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/forgot-password/forgot-password.component.ts
@@ -4,11 +4,12 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ThemeService } from '../../../core/services/theme.service';
 import { LoadingSpinnerComponent } from '../../../shared/components/loading-spinner/loading-spinner.component';
+import { AlertMessageComponent } from '../../../shared/components/alert-message/alert-message.component';
 
 @Component({
     selector: 'app-forgot-password',
     standalone: true,
-    imports: [CommonModule, ReactiveFormsModule, RouterLink, LoadingSpinnerComponent],
+    imports: [CommonModule, ReactiveFormsModule, RouterLink, LoadingSpinnerComponent, AlertMessageComponent],
     templateUrl: './forgot-password.component.html',
     styleUrl: './styles/forgot-password.component.css'
 })
@@ -16,6 +17,7 @@ export class ForgotPasswordComponent {
     emailOrLegajo = new FormControl('', [Validators.required]);
     private themeService = inject(ThemeService);
     isLoading = false;
+    successMessage: string | null = null;
 
     get logoPath(): string {
         return this.themeService.isDarkMode() ? '/logo-utn-dark-mode.png' : '/logo-utn-light-mode.png';
@@ -24,9 +26,12 @@ export class ForgotPasswordComponent {
     onSubmit() {
         if (this.emailOrLegajo.valid) {
             this.isLoading = true;
+            this.successMessage = null;
+
             setTimeout(() => {
-                console.log('Recover password for:', this.emailOrLegajo.value);
                 this.isLoading = false;
+                this.successMessage = 'Se han enviado las instrucciones de recuperación.';
+                this.emailOrLegajo.reset();
             }, 2000);
         }
     }
