774069f9e2418007507846c803272080046fabe3
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-04 22:11:26 -0300

    Navbar Component

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../../core/services/theme.service';
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+    private themeService = inject(ThemeService);
+
+    get logoPath(): string {
+        return this.themeService.isDarkMode() ? '/logo-utn-dark-mode.png' : '/logo-utn-light-mode.png';
+    }
+
+    logout(): void {
+        localStorage.removeItem('user');
+        localStorage.removeItem('token');
+        window.location.reload();
+    }
+}
