53680064ab9bbbddb2e1e94e11cf0b0f4a3d0db2
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-06 18:43:28 -0300

    NUEVO: Facultad Service

---
+++ src/app/core/services/facultad.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import axiosClient from '../api/axios.client';
+import { FacultadRequest, FacultadResponse } from '../models/facultad.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FacultadService {
+
+    constructor() { }
+
+    crearFacultad(facultad: FacultadRequest): Observable<FacultadResponse> {
+        return from(axiosClient.post<FacultadResponse>('/facultades', facultad)).pipe(
+            map(response => response.data)
+        );
+    }
+
+    listarTodas(): Observable<FacultadResponse[]> {
+        return from(axiosClient.get<FacultadResponse[]>('/facultades')).pipe(
+            map(response => response.data)
+        );
+    }
+
+    buscarPorId(id: string): Observable<FacultadResponse> {
+        return from(axiosClient.get<FacultadResponse>(`/facultades/${id}`)).pipe(
+            map(response => response.data)
+        );
+    }
+}
