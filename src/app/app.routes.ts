a3cb808c3365dc29f80a3e892f4a084f6917a336
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-07 13:34:52 -0300

    Conexion con Perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,5 +12,9 @@ export const routes: Routes = [
     {
         path: 'forgot-password',
         loadComponent: () => import('@features/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('@features/profile/profile.component').then(m => m.ProfileComponent)
     }
 ];
