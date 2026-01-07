faec710446802f871002d6588749c922d69b2499
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-06 22:59:49 -0300

    Aplico rutas absolutas enlos que faltan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ThemeToggleComponent } from './shared/components/theme-toggle.component';
+import { ThemeToggleComponent } from '@shared/components/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+        loadComponent: () => import('@features/auth/login/login.component').then(m => m.LoginComponent)
     },
     {
         path: 'dashboard',
-        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+        loadComponent: () => import('@features/dashboard/dashboard.component').then(m => m.DashboardComponent)
     },
     {
         path: 'forgot-password',
-        loadComponent: () => import('./features/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+        loadComponent: () => import('@features/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
     }
 ];
