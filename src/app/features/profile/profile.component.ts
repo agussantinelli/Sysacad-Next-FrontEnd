59fef9dc0abb660dc23cfba60d75aca2f423cc91
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-07 14:42:30 -0300

    Mejora Modal Subir Foto

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -55,6 +55,16 @@ export class ProfileComponent implements OnInit {
 
         this.usuarioService.subirFotoPerfil(this.usuario.id, file).subscribe({
             next: () => {
+                // Success: Update local state to reflect change immediately? 
+                // Since backend returns string message, we might need to reload user or just assume success?
+                // Ideally backend returns the new URL. 
+                // For now, let's just close modal and maybe reload page or re-fetch user triggered by something else?
+                // actually better to just notify user.
+
+                // Let's try to update the photo URL if we can guess it or re-fetch user
+                // The backend endpoint `subirFoto` returns a string.
+                // We should re-fetch the user data to get the new photo URL string.
+
                 this.usuarioService.obtenerPorId(this.usuario!.id).subscribe(updatedUser => {
                     this.usuario = updatedUser;
                     localStorage.setItem('user', JSON.stringify(updatedUser));
@@ -70,4 +80,3 @@ export class ProfileComponent implements OnInit {
         });
     }
 }
-
