84eace844650ba536756fc00a63f06c29cda45e6
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-15 01:55:19 -0300

    NUEVO: Salon Model

---
+++ src/app/core/models/salon.models.ts
@@ -0,0 +1,13 @@
+export interface SalonRequest {
+    idFacultad: string; // UUID
+    nombre: string;
+    piso: string;
+}
+
+export interface SalonResponse {
+    id: string; // UUID
+    idFacultad: string; // UUID
+    nombreFacultad: string;
+    nombre: string;
+    piso: string;
+}
