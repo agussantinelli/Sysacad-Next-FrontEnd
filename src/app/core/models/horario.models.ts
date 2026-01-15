04fc2969ad2e0c8c31ec84b2dd706a9e81a4655c
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-15 01:55:12 -0300

    NUEVO: Horario Model

---
+++ src/app/core/models/horario.models.ts
@@ -0,0 +1,27 @@
+export enum DiaSemana {
+    LUNES = 'LUNES',
+    MARTES = 'MARTES',
+    MIERCOLES = 'MIERCOLES',
+    JUEVES = 'JUEVES',
+    VIERNES = 'VIERNES',
+    SABADO = 'SABADO',
+    DOMINGO = 'DOMINGO'
+}
+
+export interface HorarioRequest {
+    idComision: string; // UUID
+    idMateria: string; // UUID
+    dia: DiaSemana;
+    horaDesde: string; // LocalTime
+    horaHasta: string; // LocalTime
+}
+
+export interface HorarioResponse {
+    idComision: string; // UUID
+    nombreComision: string;
+    idMateria: string; // UUID
+    nombreMateria: string;
+    dia: DiaSemana;
+    horaDesde: string; // LocalTime
+    horaHasta: string; // LocalTime
+}
