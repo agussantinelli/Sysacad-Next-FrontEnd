2cd04cf423a7579b08a933701553f9e70d086ccd
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-20 02:22:24 -0300

    NUEVO: MesaExamen Service

---
+++ src/app/core/services/mesa-examen.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import axiosClient from '@core/api/axios.client';
+import { MesaExamenRequest, MesaExamenResponse, DetalleMesaExamenRequest } from '@core/models/mesa-examen.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MesaExamenService {
+
+    constructor() { }
+
+    crearMesa(mesa: MesaExamenRequest): Observable<MesaExamenResponse> {
+        return from(axiosClient.post<MesaExamenResponse>('/mesas', mesa)).pipe(
+            map(response => response.data)
+        );
+    }
+
+    listarMesas(): Observable<MesaExamenResponse[]> {
+        return from(axiosClient.get<MesaExamenResponse[]>('/mesas')).pipe(
+            map(response => response.data)
+        );
+    }
+
+    agregarDetalleMesa(detalle: DetalleMesaExamenRequest): Observable<void> {
+        return from(axiosClient.post<void>('/mesas/detalles', detalle)).pipe(
+            map(response => response.data)
+        );
+    }
+}
