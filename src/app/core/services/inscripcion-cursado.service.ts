78b6ebc8ae254c6b5774ea14063c4481ec05e157
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-20 02:22:39 -0300

    NUEVO: InscripcionCursado Service

---
+++ src/app/core/services/inscripcion-cursado.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import axiosClient from '@core/api/axios.client';
+import { InscripcionCursadoRequest, InscripcionCursadoResponse } from '@core/models/inscripcion.models';
+import { CalificacionCursadaRequest } from '@core/models/calificacion.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class InscripcionCursadoService {
+
+    constructor() { }
+
+    inscribirCursado(inscripcion: InscripcionCursadoRequest): Observable<InscripcionCursadoResponse> {
+        return from(axiosClient.post<InscripcionCursadoResponse>('/inscripciones-cursado', inscripcion)).pipe(
+            map(response => response.data)
+        );
+    }
+
+    misCursadas(): Observable<InscripcionCursadoResponse[]> {
+        return from(axiosClient.get<InscripcionCursadoResponse[]>('/inscripciones-cursado/mis-cursadas')).pipe(
+            map(response => response.data)
+        );
+    }
+
+    cargarNotaParcial(idInscripcion: string, calificacion: CalificacionCursadaRequest): Observable<void> {
+        return from(axiosClient.post<void>(`/inscripciones-cursado/${idInscripcion}/notas`, calificacion)).pipe(
+            map(response => response.data)
+        );
+    }
+}
