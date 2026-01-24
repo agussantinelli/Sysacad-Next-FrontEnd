02489182b27c5b54347e46bbb46e90274f2d7ae0
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-24 02:39:58 -0300

    Incorporo en App el nuevo manejo de excepciones

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 import { routes } from '@/app/app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -10,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient()
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    importProvidersFrom(MatSnackBarModule)
   ]
 };
