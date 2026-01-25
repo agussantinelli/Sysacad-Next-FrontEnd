604f7c10e5a8f47215f8e1a8e6445002671b9809
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-25 02:39:33 -0300

    Modal Perfecto

--- src/app/features/student/academic-status/components/history-modal/history-modal.component.ts
+++ src/app/features/student/academic-status/components/history-modal/history-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,9 +8,15 @@ import { CommonModule } from '@angular/common';
     templateUrl: './history-modal.component.html',
     styleUrls: ['./styles/history-modal.component.css']
 })
-export class HistoryModalComponent {
+export class HistoryModalComponent implements OnInit {
     @Input() materia: any;
     @Output() close = new EventEmitter<void>();
+
+    ngOnInit(): void {
+        console.log('📊 [History Modal] Data received for:', this.materia?.nombre);
+        console.log('   - Exams:', this.materia?.historyExams);
+        console.log('   - Cursadas:', this.materia?.historyCursadas);
+    }
 
     onClose() {
         this.close.emit();
