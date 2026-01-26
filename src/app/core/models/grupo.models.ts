9a94e318e3d85c599b21929ed05ca5fadfe0cf19
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-26 02:27:52 -0300

    NUEVO: Grupo Models

---
+++ src/app/core/models/grupo.models.ts
@@ -0,0 +1,46 @@
+import { RolGrupo, EstadoGrupo } from '../enums/grupo.enums';
+
+export interface GrupoRequest {
+    nombre: string;
+    descripcion: string;
+    tipo: string;
+    idsIntegrantes: string[]; // UUIDs
+}
+
+export interface GrupoResponse {
+    id: string; // UUID
+    nombre: string;
+    descripcion: string;
+    tipo: string;
+    estado: EstadoGrupo;
+    fechaCreacion: string; // LocalDateTime
+}
+
+export interface MiembroGrupoRequest {
+    idUsuario: string; // UUID
+    rol: RolGrupo;
+}
+
+export interface MiembroGrupoResponse {
+    idUsuario: string; // UUID
+    nombre: string;
+    apellido: string;
+    rol: RolGrupo;
+    fechaUnion: string; // LocalDateTime
+    ultimoAcceso: string; // LocalDateTime
+}
+
+export interface MensajeGrupoRequest {
+    contenido: string;
+    idUsuarioRemitente?: string; // Optional (inferred from token)
+}
+
+export interface MensajeGrupoResponse {
+    id: string; // UUID
+    idGrupo: string; // UUID
+    idUsuarioRemitente: string; // UUID
+    nombreRemitente: string;
+    contenido: string;
+    editado: boolean;
+    fechaEnvio: string; // LocalDateTime
+}
