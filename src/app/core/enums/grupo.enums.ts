7e28fb919afffa5f304eff037c76aa47c5bb5081
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-26 02:28:28 -0300

    Grupo Enums

---
+++ src/app/core/enums/grupo.enums.ts
@@ -0,0 +1,10 @@
+export enum RolGrupo {
+    ADMIN = 'ADMIN',
+    MIEMBRO = 'MIEMBRO'
+}
+
+export enum EstadoGrupo {
+    ACTIVO = 'ACTIVO',
+    ARCHIVADO = 'ARCHIVADO',
+    ELIMINADO = 'ELIMINADO'
+}
