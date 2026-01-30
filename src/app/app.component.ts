4220270c96c336f625f855ddf8d661c20cf3db2e
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-01-30 18:38:09 -0300

    Incorporo Health Service en app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeToggleComponent } from '@shared/components/theme-toggle.component';
+import { HealthService } from '@core/services/health.service';
+import { AuthService } from '@core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import { ThemeToggleComponent } from '@shared/components/theme-toggle.component'
   imports: [RouterOutlet, ThemeToggleComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Sysacad-Next-FrontEnd';
+  private healthService = inject(HealthService);
+  private authService = inject(AuthService);
+
+  ngOnInit() {
+    this.authService.currentUser$.subscribe(user => {
+      if (user) {
+        this.healthService.startMonitoring();
+      } else {
+        this.healthService.stopMonitoring();
+      }
+    });
+  }
 }
