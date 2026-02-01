e27f26506217571933c5138900d3aac4558cf92d
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-01 04:03:35 -0300

    NUEVO: Mesa Modelo

---
+++ src/app/core/models/mesa-examen-disponible.models.ts
@@ -0,0 +1,9 @@
+export interface MesaExamenDisponibleDTO {
+    idDetalleMesa: string;
+    nombreMesa: string;
+    fecha: string;
+    hora: string;
+    presidente: string;
+    habilitada: boolean;
+    mensaje: string;
+}
