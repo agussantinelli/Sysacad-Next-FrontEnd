04ae636b80e4cd41908f04388c6f9e347858919b
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-02 19:13:25 -0300

    Profesor Endpoints

---
+++ src/app/core/services/professor.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import axiosClient from '@core/api/axios.client';
+import { MateriaProfesorDTO } from '@core/models/professor.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProfessorService {
+
+    constructor() { }
+
+    getMisMaterias(): Observable<MateriaProfesorDTO[]> {
+        return from(axiosClient.get<MateriaProfesorDTO[]>('/profesores/mis-materias')).pipe(
+            map(response => response.data)
+        );
+    }
+}
