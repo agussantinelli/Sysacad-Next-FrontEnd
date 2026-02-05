7e46da1e6198f1180dca63dc85f88adb1c464ed5
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-04 21:04:32 -0300

    Nuevos Endpoints Admin

---
+++ src/app/core/services/admin.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { map } from 'rxjs/operators';
+import axiosClient from '@core/api/axios.client';
+import { AdminInscripcionDTO, AdminEstadisticasDTO } from '@core/models/admin.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminService {
+
+    constructor() { }
+
+    obtenerInscripciones(): Observable<AdminInscripcionDTO[]> {
+        return from(axiosClient.get<AdminInscripcionDTO[]>('/admin/inscripciones')).pipe(
+            map(response => response.data)
+        );
+    }
+
+    obtenerEstadisticas(anio?: number, facultad?: string, carrera?: string): Observable<AdminEstadisticasDTO> {
+        const params: any = {};
+        if (anio) params.anio = anio;
+        if (facultad) params.facultad = facultad;
+        if (carrera) params.carrera = carrera;
+
+        return from(axiosClient.get<AdminEstadisticasDTO>('/admin/estadisticas', { params })).pipe(
+            map(response => response.data)
+        );
+    }
+}
