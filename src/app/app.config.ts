77d81de56e1e4c106a58901738d1127a463e1e1f
Agustin Santinelli <agustinsantinelli@gmail.com>
2026-02-05 23:45:28 -0300

    Mejora Format

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom, LOCALE_ID } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es-AR';
 
 import { routes } from '@/app/app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(localeEs, 'es-AR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([errorInterceptor])),
-    importProvidersFrom(MatSnackBarModule)
+    importProvidersFrom(MatSnackBarModule),
+    { provide: LOCALE_ID, useValue: 'es-AR' }
   ]
 };
